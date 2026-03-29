584fefdf9809128afde31957de56fe0c94960e83
Rahul <rahulkarhanakir@gmail.com>
2026-03-30 01:59:04 +0530

    final admin fix

--- build.sh
+++ build.sh
@@ -4,4 +4,6 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py migrate
 python manage.py collectstatic --noinput
-python manage.py shell -c "from django.contrib.auth import get_user_model; User=get_user_model(); username='admin'; email='admin@gmail.com'; password='admin123'; user, created = User.objects.get_or_create(username=username, defaults={'email': email}); user.email=email; user.is_staff=True; user.is_superuser=True; user.set_password(password); user.save(); print('ADMIN_READY')"
+
+# Create or update admin safely
+python manage.py shell -c "from django.contrib.auth import get_user_model; User=get_user_model(); user, created = User.objects.get_or_create(username='admin'); user.set_password('admin123'); user.is_staff=True; user.is_superuser=True; user.save(); print('ADMIN_READY')"
