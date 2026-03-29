0fea8a9cddffc7c5a88b9963dae6f979054ee7d3
Rahul <rahulkarhanakir@gmail.com>
2026-03-30 02:26:02 +0530

    keep build simple

--- build.sh
+++ build.sh
@@ -5,5 +5,3 @@ pip install -r requirements.txt
 python manage.py migrate
 python manage.py collectstatic --noinput
 
-# Create or update admin safely
-python manage.py shell -c "from django.contrib.auth import get_user_model; User=get_user_model(); user, created = User.objects.get_or_create(username='admin'); user.set_password('admin123'); user.is_staff=True; user.is_superuser=True; user.save(); print('ADMIN_READY')"
