ea1c0ec5722dd4e0a7817b1b906a16e8ab078ec8
Rahul <rahulkarhanakir@gmail.com>
2026-03-30 00:25:26 +0530

    Prepare Django project for deployment

--- build.sh
+++ build.sh
@@ -4,4 +4,6 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py migrate
 python manage.py collectstatic --noinput
-python manage.py shell -c "from django.contrib.auth import get_user_model; User=get_user_model(); username='admin'; password='admin123'; email='admin@gmail.com'; User.objects.filter(username=username).exists() or User.objects.create_superuser(username=username, email=email, password=password)"
+python manage.py shell -c "from django.contrib.auth import get_user_model; User=get_user_model(); username='admin'; password='admin123'; email='admin@gmail.com'; 
+if not User.objects.filter(username=username).exists(): 
+    User.objects.create_superuser(username=username, email=email, password=password)"
