dfef9b53577b28a3762262c2f23a8b719340c7dd
Rahul <rahulkarhanakir@gmail.com>
2026-08-10 08:47:34 +0530

    Registerd Device

--- trips/admin.py
+++ trips/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from trips.models import Trip, Vehicle, Review, Notification, DriverLocation, TripCancellation
+from trips.models import Trip, Vehicle, Review, Notification, DriverLocation, TripCancellation, DeviceToken
 
 
 @admin.register(Trip)
@@ -9,8 +9,15 @@ class TripAdmin(admin.ModelAdmin):
     search_fields = ("pickup_location", "drop_location", "employee_username", "driver_username")
 
 
+
 admin.site.register(Vehicle)
 admin.site.register(Review)
 admin.site.register(Notification)
 admin.site.register(DriverLocation)
 admin.site.register(TripCancellation)
+
+@admin.register(DeviceToken)
+class DeviceTokenAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "token")
+    list_filter = ("user",)
+    search_fields = ("token",)
