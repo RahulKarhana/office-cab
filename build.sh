1a3235dba7f1417b860bdafe90f6aedaccf21678
Rahul <rahulkarhanakir@gmail.com>
2026-09-15 11:42:05 +0530

    One-time reset Render test database

--- build.sh
+++ build.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
+
 set -o errexit
 
 pip install -r requirements.txt
+
 python manage.py migrate
+
+# ONE-TIME RESET OF RENDER DATABASE
+python manage.py flush --noinput
+
+# Recreate Render admin using existing environment variables
+python manage.py createsuperuser --noinput
+
 python manage.py collectstatic --noinput
-
-
