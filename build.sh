27f8766ae02705e20c10d045fc13f6904ccb3b71
Rahul <rahulkarhanakir@gmail.com>
2026-09-15 11:53:53 +0530

    Remove one-time database reset

--- build.sh
+++ build.sh
@@ -6,10 +6,4 @@ pip install -r requirements.txt
 
 python manage.py migrate
 
-# ONE-TIME RESET OF RENDER DATABASE
-python manage.py flush --noinput
-
-# Recreate Render admin using existing environment variables
-python manage.py createsuperuser --noinput
-
 python manage.py collectstatic --noinput
